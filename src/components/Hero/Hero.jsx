992992f0cdb65f05bc4577756f4b2ac0efcccfc5
OffBeatPixels <offbeatpixels@gmail.com>
2024-10-10 15:16:30 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
         // style={{ backgroundImage: `url(${Banner})` }}
       >
         <video
-          src="./dist/assets/food/magnet-food.webm"
+          src="./src/assets/food/magnet-food.webm"
           alt="banner-video"
           autoPlay
           muted
