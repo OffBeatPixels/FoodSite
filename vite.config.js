17c8c0fa2bc5a8919546a4db42e7efa7a5d8cb1c
Nitin-Rawat <docock0@gmail.com>
2024-10-18 15:26:32 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "docs", // Set the output directory to 'docs'
-  },
-  base: "./", // Ensures correct asset paths
 });
