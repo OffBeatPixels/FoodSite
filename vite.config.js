dbb0821b586e301352918e802563f51a520b7a4a
Nitin-Rawat <docock0@gmail.com>
2024-10-21 09:41:28 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+  build: {
+    rollupOptions: {
+      external: ["/src/assets/food/service_1.png"], // Include your image path
     },
   },
 });
